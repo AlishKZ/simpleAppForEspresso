9da2cbcb4c2ebc3a2c72032b42a7abc60bea2359
AlishKZ <a.alisher.kz2@gmail.com>
2022-03-10 00:06:42 +0300

    added lab2

--- qa_lr1/app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/SimpleTests.java
+++ qa_lr1/app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/SimpleTests.java
@@ -1,10 +1,20 @@
 package ru.kkuzmichev.simpleappforespresso;
 
+import static androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasData;
+import static org.hamcrest.Matchers.anyOf;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.espresso.IdlingRegistry;
+import org.junit.After;
+import org.junit.Before;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -25,5 +35,31 @@ public class SimpleTests {
     public void negativeCheck() {
         onView(withId(R.id.text_home))
                 .check(matches(withText("Error")));
+    }
+
+    @Test
+    public void intentCheck() {
+        Intents.init();
+        try {
+            openActionBarOverflowOrOptionsMenu(ApplicationProvider.getApplicationContext());
+        } catch (Exception e) {}
+        onView(anyOf(withText(R.string.action_settings), withId(R.id.action_settings))).perform(click());
+        Intents.intended(hasData("https://google.com"));
+    }
+
+    @Before
+    public void registerIdlingResources() { //Подключаемся к “счетчику”
+        IdlingRegistry.getInstance().register(EspressoIdlingResources.idlingResource);
+    }
+
+    @After // Выполняется после тестов
+    public void unregisterIdlingResources() { //Отключаемся от “счетчика”
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResources.idlingResource);
+    }
+
+    @Test
+    public void galleryCheck() {
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_gallery)).perform(click());
     }
 }
